d7e868bf09d992deaef6b1f24732170cc1ba9f76
HunteR6271 <honza.snejdarek@gmail.com>
2020-03-09 09:35:34 +0100

    Create Maturitní práce 5.0.py

---
+++ Maturitní práce 5.0.py
@@ -0,0 +1,39 @@
+from tkinter import *
+print("Vítám Vás u mé maturitní práce piškvorky")
+master = Tk()
+master.title("Piškvorky")
+
+print("Zadej počet řádků: ")
+a = int(input())
+print("Zadej počet sloupců: ")
+b = int(input())
+
+def velikost_pole(a,b):
+	print("Počet řádků",a ,"Počet sloupců", b)
+	for x in range(a):
+		for y in range(b):
+			button1 = Button(master, text="",command=zmena_pole , height=6, width=10)
+			button1.grid(row=x, column=y)
+	 
+turn = "X"	
+turnLabel= Label(master, text=turn, font="Helvetica 16 bold")
+turnLabel.grid(row=a, columnspan=b)
+
+def change_turn():
+    global turn
+    if turn == "O":
+        turn = "X"
+        turnLabel.config(text=turn)
+    elif turn == "X":
+        turn = "O"
+        turnLabel.config(text=turn)
+
+def zmena_pole():
+	global text
+	print ("x")
+	change_turn()
+
+velikost_pole(a,b)   
+master.mainloop()
+
+

e64b134708dbc14b40e62962a507693ea3689883
HunteR6271 <honza.snejdarek@gmail.com>
2020-04-13 19:06:01 +0200

    Update Maturitní práce.py

--- Maturitní práce.py
+++ Maturitní práce.py
@@ -3,6 +3,7 @@ print("Vítám Vás u mé maturitní práce piškvorky")
 master = Tk()
 master.title("Piškvorky")
 
+
 print("Zadej počet řádků: ")
 a = int(input())
 print("Zadej počet sloupců: ")
@@ -13,11 +14,12 @@ def velikost_pole(a,b):
 	print("Počet řádků",a ,"Počet sloupců", b)
 	for x in range(a):
 		for y in range(b):
-			button1 = Button(master, text="",command=zmena_pole , height=6, width=10)
-			button1.grid(row=x, column=y)
+			button = Button(master, text="", height=6, width=10, font="Helvetica 12 bold")
+			button.config(command = create_functions(button))
+			button.grid(row=x, column=y)
 	 
 turn = "X"	
-turnLabel= Label(master, text=turn, font="Helvetica 16 bold")
+turnLabel= Label(master, text=turn, font="Helvetica 20 bold")
 turnLabel.grid(row=a, columnspan=b)
 
 #Zobrazení kdo je na tahu.
@@ -31,12 +33,14 @@ def change_turn():
         turnLabel.config(text=turn)
 
 #Změna pole na křížek nebo kolečko po kliknutí.
-def zmena_pole():
-	global text
-	print ("x")
-	change_turn()
+def create_functions(tlacitko_funkce):
+	def zmena_pole():
+		tlacitko_funkce.config(text=turn)
+		change_turn()
+	return zmena_pole
 
 velikost_pole(a,b)   
 master.mainloop()
+
 
 

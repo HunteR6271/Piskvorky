e2d3764401484c09ffafb9cb2f7032e40f3b222b
HunteR6271 <honza.snejdarek@gmail.com>
2020-04-27 17:14:40 +0200

    Update Maturitní práce.py
    Finální verze. Hotovo pro maturitu.

--- Maturitní práce.py
+++ Maturitní práce.py
@@ -10,12 +10,16 @@ a = int(input())
 print("Zadej počet sloupců: ")
 b = int(input())
 
+#Zadání hodnoty pro výběr modu hry.
+print("Nakolik vítězných políček chceš hrát? Pokud na tři napiš 3, pokud na pět napiš 5")
+r = int(input())
+
 #Funkce vykreslení pole
 def velikost_pole(a,b):
-	print("Počet řádků",a ,"Počet sloupců", b)
+	print("Hraješ hru",a,"x",b,"na",r,"vítězných polí." )
 	for x in range(a):
 		for y in range(b):
-			button = Button(master, text="", height=6, width=10, font="Helvetica 15 bold", bg='grey', fg='white')
+			button = Button(master, text="", height=2, width=5, font="Helvetica 15 bold", bg='grey', fg='white')
 			button.config(command = create_functions(button))
 			button.grid(row=x, column=y)
 			seznam_tlacitek.append(button)
@@ -33,7 +37,10 @@ def change_turn():
 	elif turn == "X":
 		turn = "O"
 		turnLabel.config(text=turn)
-	vyhodnoceni_3()	 
+	if r == 5:
+		vyhodnoceni_5()
+	elif r == 3:
+		vyhodnoceni_3()
 
 #Funkce, která vytváří další funkce.
 def create_functions(tlacitko_funkce):
@@ -42,7 +49,8 @@ def create_functions(tlacitko_funkce):
 		tlacitko_funkce.config(text=turn)
 		change_turn()
 	return zmena_pole
-#Funkce vyhodnocení kdo vyhrál.
+
+#Funkce vyhodnocení kdo vyhrál na 3 políčka za sebou.
 def vyhodnoceni_3():
 	#Vyhrává řádek
 	for z in range(b-2):
@@ -72,27 +80,28 @@ def vyhodnoceni_3():
 	if kontrola == True:
 		print("Remíza")
 
+#Funkce vyhodnocení kdo vyhrál na 5 políček za sebou. 
 def vyhodnoceni_5():
 	#Vyhrává řádek
-	for z in range(b-2):
+	for z in range(b-4):
 		for i in range(a):
-			if seznam_tlacitek[b*i+z] ['text'] == seznam_tlacitek[b*i+1+z] ['text'] == seznam_tlacitek[b*i+2+z] ['text'] != "":
+			if seznam_tlacitek[b*i+z] ['text'] == seznam_tlacitek[b*i+1+z] ['text'] == seznam_tlacitek[b*i+2+z] ['text'] == seznam_tlacitek[b*i+3+z] ['text'] == seznam_tlacitek[b*i+4+z] ['text'] != "":
 				print (f"Vyhrál hráč {seznam_tlacitek[b*i+z] ['text']}")
 	#Vyhrává sloupec
-	for i in range(a-2):
+	for i in range(a-4):
 		for z in range(b):
-			if seznam_tlacitek[z+b*i] ['text'] == seznam_tlacitek[z+b+b*i] ['text'] == seznam_tlacitek[z+2*b+b*i] ['text'] != "":
+			if seznam_tlacitek[z+b*i] ['text'] == seznam_tlacitek[z+b+b*i] ['text'] == seznam_tlacitek[z+2*b+b*i] ['text'] == seznam_tlacitek[z+3*b+b*i] ['text'] == seznam_tlacitek[z+4*b+b*i] ['text'] != "":
 				print (f"Vyhrál hráč {seznam_tlacitek[z+b*i] ['text']}")
 	#Vyhráva diagonála z leva do prava
-	for d in range():
-		for p in range(a-3+1):
-			if seznam_tlacitek[p*b] ['text'] == seznam_tlacitek[p*b+b+1] ['text'] == seznam_tlacitek[p*b+2*b+2] ['text'] != "":
-				print (f"Vyhrál hráč {seznam_tlacitek[p*b] ['text']}")
+	for d in range(b-4):
+		for p in range(a-4):
+			if seznam_tlacitek[p*b+d] ['text'] == seznam_tlacitek[p*b+b+1+d] ['text'] == seznam_tlacitek[p*b+2*b+2+d] ['text'] == seznam_tlacitek[p*b+3*b+3+d] ['text'] == seznam_tlacitek[p*b+4*b+4+d] ['text'] != "":
+				print (f"Vyhrál hráč {seznam_tlacitek[p*b+d] ['text']}")
 	#Vyhrává diagonála z prava do leva
-	for d in range(): 
-		for k in range(a-3+1):
-			if seznam_tlacitek[k*b+2] ['text'] == seznam_tlacitek[k*b+2+b-1] ['text'] == seznam_tlacitek[k*b+2+2*b-2] ['text'] != "":
-				print (f"Vyhrál hráč {seznam_tlacitek[k*b+2] ['text']}")
+	for d in range(b-4): 
+		for k in range(a-4):
+			if seznam_tlacitek[k*b+2+d] ['text'] == seznam_tlacitek[k*b+2+b-1+d] ['text'] == seznam_tlacitek[k*b+2+2*b-2+d] ['text'] == seznam_tlacitek[k*b+2+3*b-3+d] ['text'] == seznam_tlacitek[k*b+2+4*b-4+d] ['text'] != "":
+				print (f"Vyhrál hráč {seznam_tlacitek[k*b+2+d] ['text']}")
 	#Remíza
 	kontrola = True
 	for button in seznam_tlacitek:
